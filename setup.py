b193d879a42523321a81fe60bf469034741a4699
Aria Ghora Prabono <hello@ghora.net>
2021-11-03 00:32:34 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ezpznet",
+    version="0.1.0",
+    author="Aria Ghora Prabono",
+    author_email="hello@ghora.net",
+    description="A collection of easy-to-use, ready-to-use, interesting deep neural network models",
+    license="MIT",
+    packages=["ezpznet"],
+)
