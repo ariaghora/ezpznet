d885634133a9c9188ced6521c38b39381a7da122
Aria Ghora Prabono <hello@ghora.net>
2021-11-03 00:44:04 +0700

    Update example.py

--- example.py
+++ example.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from ezpznet.animegan import AnimeGAN, load_image as animegan_load_image
 from ezpznet.sketchgan import SketchGAN, load_image as sketchgan_load_image
 
-img = sketchgan_load_image("/Users/ghora/Codes/ezpznet/large.jpg")
-net = SketchGAN()
-pred = net.predict(img)
-plt.imshow(pred.squeeze(), cmap="gray")
-plt.show()
-
-net = AnimeGAN(style="hayao")
-img = animegan_load_image("/Users/ghora/Downloads/winter.png")
-result = net.predict(img)
-plt.imshow(result)
-plt.show()
+#img = sketchgan_load_image("/Users/ghora/Codes/ezpznet/large.jpg")
+#net = SketchGAN()
+#pred = net.predict(img)
+#plt.imshow(pred.squeeze(), cmap="gray")
+#plt.show()
+#
+#net = AnimeGAN(style="hayao")
+#img = animegan_load_image("/Users/ghora/Downloads/winter.png")
+#result = net.predict(img)
+#plt.imshow(result)
+#plt.show()
